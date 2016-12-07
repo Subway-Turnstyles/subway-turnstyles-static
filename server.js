84e23359eb68240c58802d19bb9359f89b8d2dd7
Scott McCord <scott.sdm27@gmail.com>
2016-12-07 14:43:07 -0500

    set up server and file structure

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// link the routes
+const indexRoute = require('./routes/index');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// call routes
+app.use('/', indexRoute);
 
 app.listen(PORT, () => console.log ('server running at port', PORT));
