2231153fb77c6d87a0dcb8fbd5ad11521d09caba
Debika Sarkar <debikasarkar713@gmail.com>
2016-12-08 10:26:30 -0500

    calendar

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,10 +1,6 @@
 console.log('DOM loaded.')
 $( function() {
-  $("#datepicker").datepicker({
-    inline: true,
-    showOtherMonths
-    dayNameMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    });
+  $("#datepicker").datepicker();
 });
 
 
