2eca0c20e72def470db90851fe93043e1dc67ac2
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-12-08 09:44:49 -0500

    change routes

--- routes/form.js
+++ routes/form.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const { searchFlask } = require('../services/flask.js');
 
-router.get('/', (req, res) => {
+router.get('/', searchFlask, (req, res) => {
   res.render('form');
 })
 
